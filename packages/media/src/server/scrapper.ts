d91d64949aaf942190cc2bdc240efc22de96f893
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2018-04-08 17:55:16 +0200

    moved metadata

--- packages/media/src/server/scrapper.ts
+++ packages/media/src/server/scrapper.ts
@@ -1,6 +1,6 @@
 import * as akala from '@akala/server';
 import { Connection } from '@akala/json-rpc-ws'
-import { Media, MediaType, Movie, TVShow, Music } from '../metadata';
+import { Media, MediaType, Movie, TVShow, Music } from '../../metadata';
 
 export var meta = new akala.Metadata()
     .clientToServerOneWay<{ type: MediaType, priority: number }>()({ register: true })
