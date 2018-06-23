d35d7c779d05ce7cc5f41e52ddb2e5d4dd4fc3ce
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2018-06-23 06:01:58 +0200

    imported confidence method from tvdbscrapper

--- packages/media/src/server/index.ts
+++ packages/media/src/server/index.ts
@@ -2,7 +2,43 @@ import * as akala from '@akala/server';
 import { EventEmitter } from 'events';
 export { meta as scrapper } from './scrapper';
 export * from '../../metadata';
+import * as levenshtein from 'levenshtein';
 import { AssetRegistration } from '@akala-modules/core'
+
+const log = akala.log('domojs:media');
+
+export function confidence(name: string, names: string[])
+{
+    var max = 0;
+    name = name.toLowerCase().replace(/[^A-Z0-9 ]/gi, '');
+    if (names)
+    {
+        log(`${name} confidence in ${names}`);
+        akala.each(names, function (n)
+        {
+            var tokens = n.replace(/ \([0-9]{4}\)$/, '').replace(/[^A-Z0-9 ]/gi, '').toLowerCase();
+            var lev = new levenshtein(name, tokens).distance;
+            var c = 1 - lev / tokens.length;
+            if (lev < 3 && c >= max)
+            {
+                max = c;
+            }
+            var tokenArray = tokens.split(' ');
+            var match = akala.grep(tokenArray, function (token: string)
+            {
+                var indexOfToken = name.indexOf(token);
+                if (indexOfToken > 0)
+                    indexOfToken--;
+                var test = new RegExp('(?:^|\\W)' + akala.introspect.escapeRegExp(token) + '(?:$|\\W)');
+                return token.length > 0 && indexOfToken > -1 && (indexOfToken + token.length == name.length || test.test(name.substring(indexOfToken, token.length + 1)));
+            });
+            c = match.length / name.split(' ').length * match.length / tokenArray.length;
+            if (c >= max)
+                max = c;
+        });
+    }
+    return max;;
+};
 
 akala.injectWithName(['$isModule', '$master', '$worker'], function (isModule: akala.worker.IsModule, master: akala.worker.MasterRegistration, worker: EventEmitter)
 {
