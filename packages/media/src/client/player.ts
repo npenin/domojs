fca2e6e054aa3cd5788ddc3741d3c8c8754870c6
npenin <npenin@dragon-angel.fr>
2018-08-04 11:08:08 +0200

    implemented media player

---
+++ packages/media/src/client/player.ts
@@ -0,0 +1,56 @@
+import * as akala from '@akala/client'
+import { player as playerApi, controller as controllerApi } from './playerApi'
+import { api as mediaApi } from './mediaApi'
+import { Client, Connection } from '@akala/json-rpc-ws';
+import { Media } from '../../metadata';
+
+interface IScopePlayer extends akala.IScope<IScopePlayer>
+{
+    playlist?: { url: string, current?: boolean }[];
+    item?: { url: string, current?: boolean } | Media;
+    status?: { identity: string, state: 'playing' | 'paused' | 'stopped', position?: number, time?: number, length?: number };
+    controller: akala.api.Client<typeof controllerApi>;
+    togglePlaylist():void;
+    playlistVisible?:boolean;
+
+}
+
+akala.run(['$part', '$http', '$location', '$injector'], function (part: akala.Part, http: akala.Http, location: akala.LocationService, injector: akala.Injector)
+{
+    part.use('/media/player', 'body', {
+        template: '/@domojs/media/player.html', controller: function (scope: IScopePlayer, elem, params)
+        {
+            scope.togglePlaylist=function(){
+                scope.$set('playlistVisible', !scope.playlistVisible)
+            }
+
+            akala.injectWithNameAsync(['$agent.media'], async function (client: Client<Connection>)
+            {
+                var library = akala.api.jsonrpcws(mediaApi).createServerProxy(client);
+                var controller = scope.controller = akala.api.jsonrpcws(controllerApi).createClient(client,
+                    {
+                        players(players)
+                        {
+                            scope.$set('players', players);
+                        },
+                        playlist(playlist)
+                        {
+                            scope.$set('playlist', playlist);
+                            var liteMedia = playlist.playlist.find((item) => item.current);
+                            scope.$set('item', playlist.playlist.find((item) => item.current));
+                            library.getByPath(liteMedia.url).then((media) =>
+                            {
+                                if (media)
+                                    scope.$set('item', media);
+                            })
+                        },
+                        status(status)
+                        {
+                            scope.$set('status', status);
+                        }
+                    });
+                scope.$set('players', controller.$proxy().getPlayers(null));
+            })
+        }
+    });
+});
