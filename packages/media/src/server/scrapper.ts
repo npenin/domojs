b9fd4dd0af8027aba9119a0161f9f8984fd8d6e4
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2019-02-09 11:32:17 +0100

    fixed bug

--- packages/media/src/server/scrapper.ts
+++ packages/media/src/server/scrapper.ts
@@ -1,8 +1,7 @@
 import * as akala from '@akala/server';
-import { Connection } from '@akala/json-rpc-ws'
-import { Media, MediaType, Movie, TVShow, Music } from '../../metadata';
+import { Media, MediaType } from '../../metadata';
 
-export var meta = new akala.Api()
+export var scrapper = new akala.Api()
     .clientToServerOneWay<{ type: MediaType, priority: number }>()({ register: true })
     .serverToClient<Media, Media>()({ scrap: true })
     .clientToServer<Media, Media>()({ scrap: true })
