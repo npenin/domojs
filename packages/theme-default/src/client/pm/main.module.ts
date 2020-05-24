80ef90a82dbf9e5016d4bd221d41f36f7c76b415
npenin <nicolas.penin@dragon-angel.fr>
2020-05-24 10:27:44 +0000

    using resolveUrl from client export

--- packages/theme-default/src/client/pm/main.module.ts
+++ packages/theme-default/src/client/pm/main.module.ts
@@ -1,10 +1,8 @@
 import * as akala from '@akala/core'
 import * as ac from '@akala/commands'
+import * as client from '@akala/client'
 
 
 export const module = akala.module('@akala/pm', '@domojs/theme-default');
-module.injectWithName(['$resolveUrl'], function (resolveUrl)
-{
-    module.register('pm', ac.Processors.JsonRpc.connect(resolveUrl('/api/pm')).then((socket) => new ac.Container('pm', null, socket)));
-})();
+module.register('pm', ac.Processors.JsonRpc.connect(client.resolveUrl('/api/pm')).then((socket) => new ac.Container('pm', null, socket)));
 
