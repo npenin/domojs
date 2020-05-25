16c79b55fcd7373f7b994fc85aec353472340a75
npenin <nicolas.penin@dragon-angel.fr>
2020-05-25 06:59:22 +0000

    fixed server init

--- packages/theme-default/src/server/state.ts
+++ packages/theme-default/src/server/state.ts
@@ -1,3 +1,5 @@
+import { Container } from "@akala/commands";
+
 export interface ModuleDefinition
 {
     name: string
@@ -8,4 +10,5 @@ export interface State
 {
     socketPath: string;
     modules: { [key: string]: ModuleDefinition };
+    pm: Container<void>
 }
