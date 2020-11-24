d996c0ac06f3c3754b16eac24d60d16f244f0963
npenin <nicolas.penin@dragon-angel.fr>
2020-11-24 08:29:04 +0000

    fixed weather and devices impl updated icookin recipe retrieval

--- packages/devices/src/client/main.module.ts
+++ packages/devices/src/client/main.module.ts
@@ -2,14 +2,24 @@ import * as akala from '@akala/core'
 import * as ac from '@akala/commands'
 import * as web from '@domojs/theme-default'
 const deviceMetaContainer: ac.Metadata.Container = require('../../device-commands.json')
+const deviceTypeMetaContainer: ac.Metadata.Container = require('../../devicetype-commands.json')
 
-export const mdule = akala.module('@domojs/devices', '@domojs/theme-default');
+export const mdule = akala.module('@domojs/devices', '@domojs/devices/type');
 const resolveUrl = mdule.resolve('$resolveUrl');
 mdule.register('$resolveUrl', (url: string) =>
 {
-    return resolveUrl('/api/devices' + url);
+    return resolveUrl('/api/devices/' + url);
 })
 mdule.register('container', ac.proxy(deviceMetaContainer, new ac.Processors.HttpClient(mdule as any)));
 web.bootstrap.addDependency(mdule);
 
 export default mdule;
+
+
+export const mdule2 = akala.module('@domojs/devices/type', '@domojs/theme-default');
+mdule2.register('$resolveUrl', (url: string) =>
+{
+    return resolveUrl('/api/devices/types/' + url);
+})
+mdule2.register('container', ac.proxy(deviceTypeMetaContainer, new ac.Processors.HttpClient(mdule2 as any)));
+web.bootstrap.addDependency(mdule2);
