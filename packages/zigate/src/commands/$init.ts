d3c62bfc034796bf8849c2c40d6a66b79d5952db
npenin <nicolas.penin@dragon-angel.fr>
2021-07-04 16:46:00 +0000

    fixed zigate init

--- packages/zigate/src/commands/$init.ts
+++ packages/zigate/src/commands/$init.ts
@@ -1,18 +1,22 @@
 import { sidecar } from '@akala/pm';
 import { registerDeviceType } from '@domojs/devices';
 import { State } from '../state'
-
+import fs from 'fs/promises'
+import path from 'path';
 
 export default async function (this: State)
 {
     this.devices = {};
     this.devicesByAddress = {};
 
-    await registerDeviceType({
-        name: 'zigate',
-        commandMode: 'static',
-        view: (await import('../../views/device.html')).value
-    });
+
+    await fs.readFile(path.resolve(__dirname, '../../../views/new-RFY.html'), 'utf-8').then(newDeviceTemplate =>
+        registerDeviceType({
+            name: 'zigate',
+            commandMode: 'static',
+            view: newDeviceTemplate
+        })
+    );
 
     this.deviceServer = await sidecar()['@domojs/devices'];
 }
