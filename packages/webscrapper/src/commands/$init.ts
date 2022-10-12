62a3c61f18df1d1360406c0720558dd7ee0d9af1
npenin <nicolas.penin@dragon-angel.fr>
2022-10-12 04:14:13 +0000

    feat: add web scrapping support and tests

--- packages/webscrapper/src/commands/$init.ts
+++ packages/webscrapper/src/commands/$init.ts
@@ -1,29 +1,8 @@
-import { State } from "../state";
-import Configuration from '@akala/config'
-import { registerDeviceType } from "@domojs/devices";
-import { sidecar } from "@akala/pm";
-import { SerializableObject } from "@akala/json-rpc-ws";
+import State from "../state";
 import { Container } from "@akala/commands";
+import * as cheerio from 'cheerio';
 
-var state: State = null;
-
-export default async function init(this: State, container: Container<void>)
+export default async function init()
 {
-    state = this;
 
-    state.locations = (await Configuration.load("./weather.json")) || Configuration.new<SerializableObject[]>("./weather.json", []);
-
-    await registerDeviceType(container, {
-        name: 'Weather',
-        view: '@domojs/weather/new.html',
-        commandMode: 'dynamic'
-    });
-
-    const webc = await sidecar()['@akala/server'];
-
-    await webc.dispatch('remote-container', '/api/weather', require('../../commands.json'))
-
-    await webc.dispatch('asset', 'main', require.resolve('../client'));
 }
-
-init.$inject = ['container', 'options.path']
