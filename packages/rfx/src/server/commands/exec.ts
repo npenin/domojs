81ac0b863840258ea22960955539cb7168d9d2d8
Nicolas Penin <nicolas.penin@medzie.com>
2024-08-24 19:40:19 +0200

    fix: add more commands on gateway

--- packages/rfx/src/server/commands/exec.ts
+++ packages/rfx/src/server/commands/exec.ts
@@ -1,5 +1,5 @@
 import { State } from "../state.js";
-import { RFXDevice, PacketType, InterfaceControl } from "@domojs/rfx-parsers";
+import { RFXDevice, PacketType, InterfaceControl, Type } from "@domojs/rfx-parsers";
 import { Rfy } from "@domojs/rfx-parsers";
 
 export default async function (this: State, deviceName: string, command: string, value: any)
@@ -16,36 +16,49 @@ export default async function (this: State, deviceName: string, command: string,
             break;
         case PacketType.INTERFACE_CONTROL:
             var gw = await device.gateway;
-            var modes = gw.modes;
-            if (command in InterfaceControl.protocols_msg3)
+            switch (command)
             {
-                if (value)
-                    modes.msg3 = modes.msg3 | InterfaceControl.protocols_msg3[command];
-                else
-                    modes.msg3 = modes.msg3 & ~InterfaceControl.protocols_msg3[command];
+                case InterfaceControl.Commands[InterfaceControl.Commands.reset]:
+                    break;
+                case InterfaceControl.Commands[InterfaceControl.Commands.save]:
+                case InterfaceControl.Commands[InterfaceControl.Commands.status]:
+                    message = Object.assign({ command: InterfaceControl.Commands[command] }, device) as RFXDevice;
+                    break;
+                case InterfaceControl.Commands[InterfaceControl.Commands.setMode]:
+                    const modes = gw.modes;
+                    if (typeof value !== 'string')
+                        throw new Error('Invalid value');
+                    const protocol = value.substring(1);
+                    if (protocol in InterfaceControl.protocols_msg3)
+                    {
+                        if (value[0] == '+')
+                            modes.msg3 = modes.msg3 | InterfaceControl.protocols_msg3[protocol];
+                        else if (value[0] == '-')
+                            modes.msg3 = modes.msg3 & ~InterfaceControl.protocols_msg3[protocol];
+                    }
+                    if (protocol in InterfaceControl.protocols_msg4)
+                    {
+                        if (value[0] == '+')
+                            modes.msg4 = modes.msg4 | InterfaceControl.protocols_msg4[protocol];
+                        else if (value[0] == '-')
+                            modes.msg4 = modes.msg4 & ~InterfaceControl.protocols_msg4[protocol];
+                    }
+                    if (protocol in InterfaceControl.protocols_msg5)
+                    {
+                        if (value[0] == '+')
+                            modes.msg5 = modes.msg5 | InterfaceControl.protocols_msg5[protocol];
+                        else if (value[0] == '-')
+                            modes.msg5 = modes.msg5 & ~InterfaceControl.protocols_msg5[protocol];
+                    }
+                    if (protocol in InterfaceControl.protocols_msg6)
+                    {
+                        if (value[0] == '+')
+                            modes.msg6 = modes.msg6 | InterfaceControl.protocols_msg6[protocol];
+                        else if (value[0] == '-')
+                            modes.msg6 = modes.msg6 & ~InterfaceControl.protocols_msg6[protocol];
+                    }
+                    return (await device.gateway).setModes(modes);
             }
-            if (command in InterfaceControl.protocols_msg4)
-            {
-                if (value)
-                    modes.msg4 = modes.msg4 | InterfaceControl.protocols_msg4[command];
-                else
-                    modes.msg4 = modes.msg4 & ~InterfaceControl.protocols_msg4[command];
-            }
-            if (command in InterfaceControl.protocols_msg5)
-            {
-                if (value)
-                    modes.msg5 = modes.msg5 | InterfaceControl.protocols_msg5[command];
-                else
-                    modes.msg5 = modes.msg5 & ~InterfaceControl.protocols_msg5[command];
-            }
-            if (command in InterfaceControl.protocols_msg6)
-            {
-                if (value)
-                    modes.msg6 = modes.msg6 | InterfaceControl.protocols_msg6[command];
-                else
-                    modes.msg6 = modes.msg6 & ~InterfaceControl.protocols_msg6[command];
-            }
-            return (await device.gateway).setModes(modes);
             break;
     }
     if (message == null)
