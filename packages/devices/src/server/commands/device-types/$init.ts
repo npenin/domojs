a9b205aacf100b6959079ecdec8b528f946b0d11
Nicolas Penin <nicolas.penin@medzie.com>
2024-11-17 12:55:02 +0100

    fix: move config in existing config file from context

--- packages/devices/src/server/commands/device-types/$init.ts
+++ packages/devices/src/server/commands/device-types/$init.ts
@@ -1,13 +1,14 @@
 import * as devices from "../../../devices.js";
 import { Container } from "@akala/commands";
 import { sidecar, Container as pmContainer } from '@akala/pm';
-import app from "@akala/sidecar";
+import app, { SidecarConfiguration } from "@akala/sidecar";
 import { DbSet } from "@akala/storage";
-import { CliContext } from "@akala/cli";
+import { CliContext, OptionType } from "@akala/cli";
 import { fileURLToPath } from 'url'
+import { ProxyConfiguration } from "@akala/config";
 
 
-export default async function (this: devices.DeviceTypeState, context: CliContext, container: Container<unknown>, pm: pmContainer & Container<any>)
+export default async function (this: devices.DeviceTypeState, context: CliContext<Record<string, OptionType>, ProxyConfiguration<{ devicetype: {} }>>, container: Container<unknown>, pm: pmContainer & Container<any>)
 {
     debugger;
     try
@@ -23,5 +24,7 @@ export default async function (this: devices.DeviceTypeState, context: CliContex
     }
     this.initializing = [];
     this.types = {};
-    Object.assign(this, await app<{ DeviceInit: DbSet<{ name: string, body: any }> }>(context, fileURLToPath(new URL('../../../../devicetype-app.json', import.meta.url)), pm));
+    if (!context.state.has('devicetype'))
+        context.state.set('devicetype', {});
+    Object.assign(this, await app<{ DeviceInit: DbSet<{ name: string, body: any }> }>(context, context.state.devicetype, pm));
 }
