fa4492bfbdb6bedd4a5e208cf64e9ad33fac0ab1
npenin <github@dragon-angel.fr>
2025-08-04 09:54:14 +0200

    chore: update akala

--- packages/homescreen/client/pages/home/home.ts
+++ packages/homescreen/client/pages/home/home.ts
@@ -4,8 +4,10 @@ import template from './home.html?raw'
 @page({ template, 'inject': [RootElement] })
 export default class Home extends Page
 {
-    public readonly carrefour = Promise.resolve({ displayName: 'Carrefour', currentOpeningHours: { openNow: true } });
-    public readonly lidl = Promise.resolve({ displayName: 'Lidl', currentOpeningHours: { openNow: true } });
+    public readonly carrefour =
+        fetch('https://places.googleapis.com/v1/places/ChIJDyoiLdOEkUcRItC5kmfPzA0?fields=displayName,currentOpeningHours&key=AIzaSyC6zSNgmdh4AM1QUVlG0TMs4F-Wp-QPsGY').then(res => res.json());
+    public readonly lidl =
+        fetch('https://places.googleapis.com/v1/places/ChIJN7vPltGEkUcRjgTfwgs8BVo?fields=displayName,currentOpeningHours&key=AIzaSyC6zSNgmdh4AM1QUVlG0TMs4F-Wp-QPsGY').then(res => res.json());
 
     constructor(private el: HTMLElement)
     {
