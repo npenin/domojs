987ca3caf8aa6baec6ac53bac4d14fbdb4a14244
npenin <github@dragon-angel.fr>
2025-08-04 09:55:50 +0200

    fix: devices first setup

--- packages/devices/src/server/commands/devices/$init.ts
+++ packages/devices/src/server/commands/devices/$init.ts
@@ -1,10 +1,10 @@
 import { CliContext } from "@akala/cli";
 import { Container } from "@akala/commands";
 import Configuration, { ProxyConfiguration } from "@akala/config";
-import app, { Sidecar, SidecarConfiguration } from "@akala/sidecar";
+import app, { pubsub, Sidecar, SidecarConfiguration } from "@akala/sidecar";
 import { Container as pmContainer } from '@akala/pm'
 // import { Commissionner, FabricServer, FabricClient } from "../../clients/index.js";
-import { MqttEvents } from "@domojs/mqtt";
+import { MqttClient, MqttEvents } from "@domojs/mqtt";
 import devices from '../../device-commands.js'
 import { Endpoint, EndpointProxy } from "../../clients/Endpoint.js";
 import Commissionnee from "../../clusters/Commissionnee.js";
@@ -34,54 +34,54 @@ export default async function (this: State, context: CliContext<{ configFile: st
     if (!context.state.has('endpointsMapping'))
         context.state.set('endpointsMapping', {});
 
-    if (!this.config.has('store'))
-    {
-        context.state.set('store', {
-            "provider": "file+json://./",
-            "models": {
-                "DeviceInit": {
-                    "members": {
-                        "name": {
-                            "isKey": true,
-                            "type": {
-                                "type": "string",
-                                "length": 250
-                            },
-                            "generator": "business"
-                        },
-                        "body": {
-                            "isKey": false,
-                            "type": {
-                                "type": "string"
-                            }
-                        }
-                    }
-                }
-            }
-        });
+    // if (!this.config.has('store'))
+    // {
+    //     context.state.set('store', {
+    //         "provider": "file+json://./",
+    //         "models": {
+    //             "DeviceInit": {
+    //                 "members": {
+    //                     "name": {
+    //                         "isKey": true,
+    //                         "type": {
+    //                             "type": "string",
+    //                             "length": 250
+    //                         },
+    //                         "generator": "business"
+    //                     },
+    //                     "body": {
+    //                         "isKey": false,
+    //                         "type": {
+    //                             "type": "string"
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     });
 
-    }
+    // }
 
-    const sidecar = Object.assign(this, await app(context, pm));
+    const sidecar = Object.assign(this, await app<{}, MqttEvents>(context, pm));
     sidecar.sidecars['@domojs/devices'] = Promise.resolve(container);
 
     await context.state.commit();
 
+    const commissionner = Commissionnee(this);
+
+    const [pubsubConfig] = await commissionner.target.registerCommand('devices');
+    if (pubsubConfig)
+    {
+        await (sidecar.pubsub as MqttClient).disconnect();
+        delete sidecar.pubsub;
+        delete sidecar.config.pubsub;
+        await pubsub(sidecar, pubsubConfig);
+    }
+
+
     this.self = new RootNode('devices', {
-        commissionning: Commissionnee(this),
+        commissionning: commissionner,
     }, context.state);
 
     await this.self.attach(sidecar.pubsub);
-
-
-    const [pubsubConfig] = await this.self.clusters.commissionning.target.registerCommand('devices');
-    if (pubsubConfig)
-    {
-        context.state.set('pubsub', pubsubConfig);
-        await context.state.pubsub.transportOptions.setSecret('password', context.state.pubsub.transportOptions.get('passport'));
-        await context.state.commit();
-    }
-
-    // await FabricServer.register('devices', this, new Commissionner(this, 'devices'));
-
 }
