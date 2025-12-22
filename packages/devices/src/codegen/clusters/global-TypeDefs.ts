2e42576315c9ec2b126bac57a94747b5f5d0e5a4
npenin <github@dragon-angel.fr>
2025-12-22 11:25:49 +0100

    style: remove old files for renaming

--- packages/devices/src/codegen/clusters/global-TypeDefs.ts
+++
@@ -1,7 +0,0 @@
-// This file is generated from global-TypeDefs.xml - do not edit it directly
-// Generated on 2025-12-22T10:19:22.926Z
-
-import { Cluster, ClusterDefinition } from '../../server/clients/shared.js';
-
-
-export type WebRTCSessionID = number;
